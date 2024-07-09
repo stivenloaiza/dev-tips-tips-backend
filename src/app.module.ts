8027a521fc07a0aadfb60604dfc60da5a29638e7
gelhg08 <angelicamhg31@gmail.com>
2024-07-09 06:53:50 -0500

    Created entity and DTOs for Tips feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersistenceModule } from './libs/persistence/persistence.module';
 import { ConfigModule } from '@nestjs/config';
-import  dbConfig  from './libs/persistence/dbConfig';
+import { TipsModule } from './modules/tips/tips.module';
+import dbConfig from './libs/persistence/dbConfig';
 
 @Module({
   imports: [
-      ConfigModule.forRoot({
-        envFilePath: '.env',
-        load: [dbConfig],
-        isGlobal: true,
-      }),
-    PersistenceModule],
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    PersistenceModule,
+    TipsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
--- src/modules/tips/dto/create-tip.dto.ts
+++ src/modules/tips/dto/create-tip.dto.ts
@@ -1,1 +1,72 @@
-export class CreateTipDto {}
+import { IsNumber, IsOptional, IsString, IsBoolean, IsDateString, ArrayNotEmpty, ArrayMinSize, IsMongoId } from 'class-validator';
+
+export class CreateTipDto {
+  @IsNumber()
+  id: number;
+
+  @IsOptional()
+  @IsString()
+  img_url?: string;
+
+  @IsString()
+  title: string;
+
+  @IsString()
+  body: string;
+
+  @IsOptional()
+  @IsString()
+  link?: string;
+
+  @IsBoolean()
+  available: boolean;
+
+  @ArrayNotEmpty()
+  @IsMongoId()
+  @IsString({ each: true })
+  level: string[];
+
+  @ArrayNotEmpty()
+  @IsMongoId({each: true})
+  @IsString({ each: true }) 
+  technology: string[];
+
+  @ArrayNotEmpty()
+  @IsMongoId({ each: true })
+  @IsString({ each: true })
+  subtechnology: string[];
+
+  @ArrayNotEmpty()
+  @IsMongoId({ each: true }) 
+  @IsString({ each: true }) 
+  lang: string[];
+
+  @IsOptional()
+  @IsDateString()
+  createdAt?: Date;
+
+  @IsOptional()
+  @IsString()
+  createBy?: string;
+
+  @IsOptional()
+  @IsDateString()
+  updatedAt?: Date;
+
+  @IsOptional()
+  @IsString()
+  updateBy?: string;
+
+  @IsOptional()
+  @IsDateString()
+  deletedAt?: Date;
+
+  @IsOptional()
+  @IsString()
+  deleteBy?: string;
+}
+
+
+
+
+
